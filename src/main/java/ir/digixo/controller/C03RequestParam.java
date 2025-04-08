f32b2fdee82cbf752fb06d948457a0b89dadd830
Nima Foroohari <nforoohari@gmail.com>
2025-04-08 20:04:10 +0330

    Version 4

--- src/main/java/ir/digixo/controller/C03RequestParam.java
+++ src/main/java/ir/digixo/controller/C03RequestParam.java
@@ -1,49 +1,42 @@
 package ir.digixo.controller;
 
-import ir.digixo.entity.Product;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import ir.digixo.entity.Product;
 
 @Controller
 @RequestMapping("/3")
 public class C03RequestParam {
+
     //http://localhost:8080/3/m1?id=12&name=iphone
     @GetMapping("/m1")
-    ModelAndView m1(HttpServletRequest request)
-    {
-
-        // request.setAttribute("name","leila");
+    ModelAndView m1(HttpServletRequest request) {
 
         String id = request.getParameter("id");
         String name = request.getParameter("name");
 
-        Product p=new Product(Long.parseLong(id),name);
+        Product myProduct = new Product(Long.parseLong(id), name);
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
-        modelAndView.addObject("p",p);
+        modelAndView.setViewName("home3");
+        modelAndView.addObject("my_product", myProduct);
         return modelAndView;
     }
+
+    //http://localhost:8080/3/m2?id=3&name=samsung
     @GetMapping("/m2")
-    ModelAndView m2(@RequestParam(value = "id") Long id,@RequestParam(value = "name",required = false,defaultValue = "sib") String name)
-    {
+    ModelAndView m2(@RequestParam(value = "id") Long id, @RequestParam(value = "name", required = false, defaultValue = "nothing") String name) {
 
-        // request.setAttribute("name","leila");
-
-       /* String id = request.getParameter("id");
-        String name = request.getParameter("name");*/
-
-        Product p=new Product(id,name);
+        Product myProduct = new Product(id, name);
 
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("home");
-        modelAndView.addObject("p",p);
+        modelAndView.setViewName("home3");
+        modelAndView.addObject("my_product", myProduct);
         return modelAndView;
     }
-
 
 }
